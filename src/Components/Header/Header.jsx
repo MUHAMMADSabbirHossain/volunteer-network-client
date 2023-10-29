bc23da13c6252d9a21a0b7d97ec6ea438372e121
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-29 12:39:39 +0600

    component: navBar added and linked

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Header.css";
+import NavBar from '../NavBar/NavBar';
 
 const Header = () => {
     return (
         <div>
             <h1>Header</h1>
+            <NavBar></NavBar>
         </div>
     );
 };

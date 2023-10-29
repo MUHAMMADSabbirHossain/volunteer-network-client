cea07c4b2bef1fa325bfe3266248af42144387fb
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-29 14:16:30 +0600

    features, component: home components edited and volunteers components added.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./Home.css";
+import Volunteers from '../Volunteers/Volunteers';
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <h1>I GROW BY HELPING PEOPLE IN NEED.</h1>
+            <input type="text" name="" id="" placeholder="Search... " />
+            <button type="submit">Search</button>
+
+            <Volunteers></Volunteers>
         </div>
     );
 };
---
+++ src/Components/Volunteers/Volunteers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Volunteers.css";
+
+const Volunteers = () => {
+    return (
+        <div>
+            <h1>Volunteers</h1>
+        </div>
+    );
+};
+
+export default Volunteers;

38aeb466139b441ed6f0bc625a43d1274404934d
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-29 14:43:48 +0600

    feature, signin: image added.

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -6,11 +6,14 @@ const SignIn = () => {
     return (
         <div>
             <h1>Sign In</h1>
-            <h2>Login With</h2>
+            <img src="../../assets/logos/Group 1329.png" alt="Volunteer Network logo" />
             <div>
-                <p>Continue with Google</p>
+                <h2>Login With</h2>
+                <div>
+                    <p>Continue with Google</p>
+                </div>
+                <p>Don't have an account? <Link to="/signup">Register Now.</Link></p>
             </div>
-            <p>Don't have an account? <Link to="/signup">Register Now.</Link></p>
         </div>
     );
 };

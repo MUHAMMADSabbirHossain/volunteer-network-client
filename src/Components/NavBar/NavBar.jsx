5fb412cfc9e00d5857261b2d200e283062161eb0
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-29 13:57:07 +0600

    navbar, Link: added.

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./NavBar.css";
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div>
             <h1>NavBar</h1>
+            <nav>
+                <div>
+                    <Link to="/">
+                        <img src="../../assets/logos/Group 1329.png" alt="" />
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/home">Home</Link>
+                    <Link to="/donation">Donation</Link>
+                    <Link to="/events">Events</Link>
+                    <Link to="/blogs">Blogs</Link>
+                    <Link to="/register"> Register</Link>
+                    <Link to="/signin">Sign In</Link>
+                    <Link to="/signout">Sing Out</Link>
+                    <Link to="/admin">Admin</Link>
+                </div>
+            </nav>
         </div>
     );
 };

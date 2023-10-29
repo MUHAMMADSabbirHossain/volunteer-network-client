923958298d696691930c203fd2a23dbe7cd2567b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-29 14:40:14 +0600

    feature, signin: signin edited.

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./SignIn.css";
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
     return (
         <div>
             <h1>Sign In</h1>
+            <h2>Login With</h2>
+            <div>
+                <p>Continue with Google</p>
+            </div>
+            <p>Don't have an account? <Link to="/signup">Register Now.</Link></p>
         </div>
     );
 };

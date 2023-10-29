4b668d5e6434be761d4cc9fba4ec33bcfdbd5d0e
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-29 14:34:37 +0600

    navbar, image: alt added.

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -9,16 +9,16 @@ const NavBar = () => {
             <nav>
                 <div>
                     <Link to="/">
-                        <img src="../../assets/logos/Group 1329.png" alt="" />
                     </Link>
+                    <img src="../../assets/logos/Group 1329.png" alt="Volunteer Network logo" />
                 </div>
                 <div>
                     <Link to="/">Home</Link>
                     <Link to="/donation">Donation</Link>
                     <Link to="/events">Events</Link>
                     <Link to="/blogs">Blogs</Link>
-                    <Link to="/signup">Sign Up</Link>
-                    <Link to="/signin">Sign In</Link>
+                    <Link to="/signup">Sign-Up</Link>
+                    <Link to="/signin">Sign-In</Link>
                     {/* <Link to="/signout">Sing Out</Link> */}
                     <Link to="/admin">Admin</Link>
                 </div>
